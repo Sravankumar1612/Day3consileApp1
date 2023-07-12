ccc6f788a5ef4448a42f61acae896e73af80678b
Sravankumar1612 <sravankumartikkavarapu@gmail.com>
2023-07-12 14:24:07 +0530

    second commit

--- Program.cs
+++ Program.cs
@@ -8,8 +8,28 @@ namespace ConsoleApp1
 {
     internal class Program
     {
+        enum Monthsofyear { jan = 1, feb = 2, mar = 3 }
         static void Main(string[] args)
         {
+            Console.WriteLine("First Month: \t " + (int)Monthsofyear.jan);
+            Console.WriteLine("Second Month: \t" + Monthsofyear.feb);
+            Console.ReadKey();
+            //byte myByte = 12;
+            //sbyte myByte2 = 13;
+            //short mySShort = 12;
+            try
+            {
+                byte ourByte;
+                Console.WriteLine("Enter a number");
+
+                ourByte = Convert.ToByte(Console.ReadLine());
+                Console.WriteLine("Nuber is " + ourByte);
+            }
+            catch (FormatException ex) { Console.WriteLine("Format exception : "+ex); }
+            catch (OverflowException oe) { Console.WriteLine("Overflow exception"+oe); }
+            catch (Exception e) { Console.WriteLine("Error : " + e); }
+            finally { Console.WriteLine("End of Program"); }
+            Console.ReadKey();
         }
     }
 }
